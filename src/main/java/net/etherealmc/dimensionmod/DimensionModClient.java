a30f712a6e949bd92006c16d52668d632e6f9ea2
BuildTools <unconfigured@null.spigotmc.org>
2024-01-22 00:43:44 +0000

    Initial commit

---
+++ src/main/java/net/etherealmc/dimensionmod/DimensionModClient.java
@@ -0,0 +1,10 @@
+package net.etherealmc.dimensionmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DimensionModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
